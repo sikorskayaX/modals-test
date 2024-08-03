c7adebf3dd096ddd62e22dc106bd6887c7d8726c
sikorskayaX <sikorskaya02@gmail.com>
2024-08-03 05:16:17 +0300

    Style for input range

--- src/components/EmployeeInputRange.jsx
+++ src/components/EmployeeInputRange.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
 const EmployeeInputRange = ({minEmployeeCount, maxEmployeeCount}) =>{
-	const [employeeCount, setEmployeeCount] = useState(1);
+	const [employeeCount, setEmployeeCount] = useState(1); 
 
 	const handleEmployeeCountChange = (event) => {
 		setEmployeeCount(event.target.value);
 	};
-	
+
+	const progressPercentage = ((employeeCount - 1) / (maxEmployeeCount - 1)) * 100;
+
 	return(			
 			<form className="form-range">
-			  <label className="form-range__label"
-				//style={{ backgroundSize: `${(employeeCount - {minEmployeeCount}) / ({maxEmployeeCount} - 100) * 100}%`}}
-				>
 				<input
 				  className="form-range__input"
 				  type="range"
@@ -19,11 +18,17 @@ const EmployeeInputRange = ({minEmployeeCount, maxEmployeeCount}) =>{
 				  max={maxEmployeeCount}
 				  value={employeeCount}
 				  onChange={handleEmployeeCountChange}
+				  style={{
+					background: `linear-gradient(to right, #9FC136 ${progressPercentage}%, #D9D9D9 ${progressPercentage}%)`
+				  }}
 				/>
 				<div 
-				className="form-range__label-value"
-				></div>
-			  </label>
+					className="form-range__label-value"
+					style={{
+                        left: `${progressPercentage}%`,
+                        display: employeeCount == minEmployeeCount || employeeCount == maxEmployeeCount ? 'none' : 'block'
+                      }}
+				>{employeeCount}</div>
 			  <div className='form-range__values'>
 				<span>{minEmployeeCount}</span>
 				<span>{maxEmployeeCount}</span>
